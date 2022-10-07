2d50c5f1b6902a1bf034212ce282a99a8b1f892b
Mike Patek <mpatek@gmail.com>
2022-10-07 11:52:52 -0400

    Create word feature model

--- rlev.py
+++ rlev.py
@@ -1,10 +1,17 @@
 import pickle
 
 import click
+import numpy as np
+from scipy import sparse
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
 
 # Minimum document frequency for word features
 DEFAULT_WORD_FEATURE_MIN_DF = 250
+
+# Minimum number of word features for document
+# to be included when building word feature model
+DEFAULT_WORD_FEATURE_MIN_FEATURES = 2
 
 
 @click.group()
@@ -34,6 +41,96 @@ def create_count_vectorizer(
     )
     vectorizer.fit(infile)
     pickle.dump(vectorizer, outfile)
+
+
+def chunks(items, n):
+    curr_chunk = []
+    for item in items:
+        curr_chunk.append(item)
+        if len(curr_chunk) == n:
+            yield curr_chunk
+            curr_chunk = []
+    if curr_chunk:
+        yield curr_chunk
+
+
+@cli.command("create-word-feature-model-inputs")
+@click.argument("infile", type=click.File("rt"))
+@click.argument("outfile", type=click.File("wb"))
+@click.option("--title-vectorizer", type=click.File("rb"), required=True)
+@click.option("--abstr-vectorizer", type=click.File("rb"), required=True)
+@click.option(
+    "--min-word-features",
+    type=int,
+    default=DEFAULT_WORD_FEATURE_MIN_FEATURES,
+    help="Minimum number of word features for a document to be "
+    "included in output matrix/labels.",
+)
+def create_word_feature_model_inputs(
+    infile,
+    outfile,
+    title_vectorizer,
+    abstr_vectorizer,
+    min_word_features,
+):
+    """Create the input matrix and label array for a word feature model."""
+
+    title_vectorizer = pickle.load(title_vectorizer)
+    abstr_vectorizer = pickle.load(abstr_vectorizer)
+
+    Y = None
+    X = None
+
+    line_chunks = chunks(infile, 100000)
+    for chunk in line_chunks:
+        fields = [line.strip().split("\t") for line in chunk]
+
+        # Only select rows where we can extract 3 fields.
+        fields = [f for f in fields if len(f) == 3]
+
+        title_features = title_vectorizer.transform(f[1] for f in fields)
+        abstr_features = abstr_vectorizer.transform(f[2] for f in fields)
+        features = sparse.hstack((title_features, abstr_features))
+
+        # Subtract 1 since input rlevs are 1-indexed
+        rlevs = np.array([int(f[0]) - 1 for f in fields])
+
+        row_sums = features.sum(axis=1)
+        wc_mask = np.greater_equal(row_sums, min_word_features).A1
+
+        features = features[wc_mask, :]
+        rlevs = rlevs[wc_mask]
+
+        if X is None:
+            X = features
+        else:
+            X = sparse.vstack((X, features))
+
+        if Y is None:
+            Y = rlevs
+        else:
+            Y = np.hstack((Y, rlevs))
+
+    print("X:", X.shape, type(X))
+    print("Y:", Y.shape, type(Y))
+    pickle.dump((X, Y), outfile)
+
+
+@cli.command("train-word-feature-model")
+@click.argument("infile", type=click.File("rb"))
+@click.argument("outfile", type=click.File("wb"))
+def train_word_feature_model(
+    infile,
+    outfile,
+):
+    """Train word feature model."""
+    X, Y = pickle.load(infile)
+    model = LogisticRegression(
+        penalty="l2",
+        C=1e5,
+    )  # C=1e2, max_iter=350)
+    model.fit(X, Y)
+    pickle.dump(model, outfile)
 
 
 if __name__ == "__main__":
--- rlev.sh
+++ rlev.sh
@@ -1,30 +1,51 @@
 #!/bin/bash
 
+set -e
+
 RLEV="python rlev.py"
 DATA_DIR="data"
-MIN_WORD_FEATURE_DF=10
+MIN_WORD_FEATURE_DF=250
 MIN_YR=10
-MAX_YR=10
+MAX_YR=13
+
+TITLE_COL=8
+ABSTR_COL=9
+RLEV_COL=2
 
 mkdir -p $DATA_DIR
 
 echo "Downloading data from S3..."
+ALL_INPUT_FILES=""
 for yr in $(seq $MIN_YR $MAX_YR)
 do
     echo $yr
-    # aws s3 cp "s3://scitech/projects/rlev/cpid${yr}_rl_nr_fx_t_a.txt.gz" $DATA_DIR
+    basename="cpid${yr}_rl_nr_fx_t_a.txt.gz"
+    # aws s3 cp "s3://scitech/projects/rlev/$basename" $DATA_DIR
+    ALL_INPUT_FILES="$ALL_INPUT_FILES $DATA_DIR/$basename"
 done
+
+UNZIP_INPUT="gunzip -c $ALL_INPUT_FILES"  #  | head -1000000"
 
 echo "Building title vectorizer..."
 TITLE_VECTORIZER="$DATA_DIR/title-vectorizer.pickle"
-gunzip -c $DATA_DIR/cpid*_rl_nr_fx_t_a.txt.gz | \
-    awk 'BEGIN{FS="\t"}{print $8}' | \
-    head -10000 | \
+eval $UNZIP_INPUT | \
+    awk 'BEGIN{FS="\t"}{print $'$TITLE_COL'}' | \
     $RLEV create-count-vectorizer - "$TITLE_VECTORIZER" --min-df "$MIN_WORD_FEATURE_DF"
 
 echo "Building abstract vectorizer..."
 ABSTR_VECTORIZER="$DATA_DIR/abstr-vectorizer.pickle"
-gunzip -c $DATA_DIR/cpid*_rl_nr_fx_t_a.txt.gz | \
-    awk 'BEGIN{FS="\t"}{print $9}' | \
-    head -10000 | \
+eval $UNZIP_INPUT | \
+    awk 'BEGIN{FS="\t"}{print $'$ABSTR_COL'}' | \
     $RLEV create-count-vectorizer - "$ABSTR_VECTORIZER" --min-df "$MIN_WORD_FEATURE_DF"
+
+echo "Building word feature model inputs..."
+WORD_FEATURE_INPUTS="$DATA_DIR/word-feature-model-inputs.pickle"
+eval $UNZIP_INPUT | \
+    awk 'BEGIN{FS="\t";OFS="\t"}{print $'$RLEV_COL',$'$TITLE_COL',$'$ABSTR_COL'}' | \
+    $RLEV create-word-feature-model-inputs - "$WORD_FEATURE_INPUTS" \
+    --title-vectorizer "$TITLE_VECTORIZER" \
+    --abstr-vectorizer "$ABSTR_VECTORIZER"
+
+echo "Training model..."
+WORD_FEATURE_MODEL="$DATA_DIR/word-feature-model.pickle"
+$RLEV train-word-feature-model "$WORD_FEATURE_INPUTS" "$WORD_FEATURE_MODEL"
