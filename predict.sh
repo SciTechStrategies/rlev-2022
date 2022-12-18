e00990f0350297ed0af235a75cd068a79a70f0a2
Mike Patek <mpatek@gmail.com>
2022-12-18 09:37:30 -0500

    Use model dir in predict script

--- predict.sh
+++ predict.sh
@@ -7,11 +7,11 @@ INPUT_FILE="${1:-/dev/stdin}"
 MODEL_DIR="${MODEL_DIR:-model}"
 
 RLEV="python rlev.py"
-TITLE_VECTORIZER="$DATA_DIR/title-vectorizer.pickle"
-ABSTR_VECTORIZER="$DATA_DIR/abstr-vectorizer.pickle"
-WORD_FEATURE_MODEL="$DATA_DIR/word-feature-model.pickle"
-RLEV_PRIORS="$DATA_DIR/rlev-priors.pickle"
-COMBINED_MODEL="$DATA_DIR/combined-model.pickle"
+TITLE_VECTORIZER="$MODEL_DIR/title-vectorizer.pickle"
+ABSTR_VECTORIZER="$MODEL_DIR/abstr-vectorizer.pickle"
+WORD_FEATURE_MODEL="$MODEL_DIR/word-feature-model.pickle"
+RLEV_PRIORS="$MODEL_DIR/rlev-priors.pickle"
+COMBINED_MODEL="$MODEL_DIR/combined-model.pickle"
 
 $RLEV get-combined-model-predictions "$INPUT_FILE" \
     --title-vectorizer "$TITLE_VECTORIZER" \
