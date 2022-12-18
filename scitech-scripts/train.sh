48ecb70cd1304590b1c2147becde987f2d8a036f
Mike Patek <mpatek@gmail.com>
2022-12-18 09:38:49 -0500

    Remove unused column in scitech train script

--- scitech-scripts/train.sh
+++ scitech-scripts/train.sh
@@ -7,4 +7,6 @@ export DATA_DIR
 MODEL_DIR=rlev-model
 export MODEL_DIR
 
-gunzip -c "$DATA_DIR"/cpid*_rl_nr_fx_t_a.txt.gz | ./train.sh
+gunzip -c "$DATA_DIR"/cpid*_rl_nr_fx_t_a.txt.gz | \
+    awk 'BEGIN{FS=OFS="\t"}{print $1,$2,$4,$5,$6,$7,$8,$9}' | \
+    ./train.sh
